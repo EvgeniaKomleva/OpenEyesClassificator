4c29f564eb29b6f48ab7b511d010711aba406c9c
EvgeniaKomleva <36705500+EvgeniaKomleva@users.noreply.github.com>
2023-03-13 16:56:26 +0400

    Update train.py

--- train.py
+++ train.py
@@ -1,49 +1,28 @@
 import os
-import shutil
 import numpy as np
 from PIL import Image
-import cv2
-
+from sklearn.metrics import roc_curve
 import torch
 import torch.nn as nn
 import torch.optim as optim
-# from torch.optim.lr_scheduler import StepLR
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import torchvision
-from torchvision import transforms
 import torchvision.models as models
+from torchvision.models import ResNet18_Weights, ResNet34_Weights,Wide_ResNet50_2_Weights, ResNet50_Weights
 import wandb
 
+
 # ________DATA_______________________________________________________________________________
-train_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(256),
-    transforms.RandomHorizontalFlip(p=0.2),
-    transforms.RandomRotation(degrees=(-45, 45)),
-    transforms.RandomPerspective(distortion_scale=0.7, p=1, interpolation=2, fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.4976, 0.4976, 0.4976],
-                         std=[0.1970, 0.1970, 0.1970]),
-    ])
+weights = ResNet34_Weights.IMAGENET1K_V1
+train_transform=weights.transforms()
+print(train_transform)
 
-val_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(256),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.4976, 0.4976, 0.4976],
-                          std=[0.1970, 0.1970, 0.1970])
-    ])
 
-BATCH_SIZE = 64
-DATA_DIR = '/Users/evgenia/OpenEyesClassificator/'
+BATCH_SIZE = 128
+DATA_DIR = './'
 
 dataset = torchvision.datasets.ImageFolder(root=os.path.join(DATA_DIR, 'eyes'), transform=train_transform)
-
-n = len(dataset)  # total number of examples
-n_test = int(0.1 * n)  # take ~10% for test
-test_set = torch.utils.data.Subset(dataset, range(n_test))  # take first 10%
-val_set =  torch.utils.data.Subset(dataset, range(n_test, 2*n_test))  # take second 10%
-train_set = torch.utils.data.Subset(dataset, range(2*n_test, n))  # take the rest   
+train_set,val_set = torch.utils.data.random_split(dataset, [0.85, 0.15])
 
 train_loader = DataLoader(train_set, 
                           batch_size=BATCH_SIZE, 
@@ -55,50 +34,35 @@ val_loader = DataLoader(val_set,
                         shuffle=True, 
                         num_workers=0) 
 
-test_loader = DataLoader(test_set, 
-                         batch_size=BATCH_SIZE, 
-                         shuffle=True, 
-                         num_workers=0) 
+
 
 
 # ________TRAIN_FUNCTION_______________________________________________________________________________
 
                     
 def compute_eer(labels, scores):
-    """Compute the Equal Error Rate (EER) from the predictions and scores.
-    Args:
-        labels (list[int]): values indicating whether the ground truth
-            value is positive (1) or negative (0).
-        scores (list[float]): the confidence of the prediction that the
-            given sample is a positive.
-    Return:
-        (float, thresh): the Equal Error Rate and the corresponding threshold
-    NOTES:
-       The EER corresponds to the point on the ROC curve that intersects
-       the line given by the equation 1 = FPR + TPR.
-       The implementation of the function was taken from here:
-       https://yangcha.github.io/EER-ROC/
-    """
     fpr, tpr, thresholds = roc_curve(labels, scores, pos_label=1)
-    eer = brentq(lambda x : 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
-    thresh = interp1d(fpr, thresholds)(eer)
+    fnr = 1 - tpr
+    eer_index = np.argmin(np.abs(fnr - fpr))
+    eer = (fpr[eer_index] + fnr[eer_index]) / 2
+    thresh = thresholds[eer_index]
     return eer, thresh
+    
 
-def eval(model, loader, ckpt_path=False):
+def evaluate(model, loader, criterion, device, ckpt_path=None):
     if ckpt_path:
         model.load_state_dict(torch.load(ckpt_path))
 
+    model.eval()
     val_loss = 0.0
-    total = 0
     correct = 0
+    total = 0
     val_labels = []
     val_probs = []
-    model.eval()  
 
     with torch.no_grad():
-        for batch in loader:
-            inputs, labels = batch[0].to(device), batch[1].to(device)
-          
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             val_loss += loss.item()
@@ -112,54 +76,81 @@ def eval(model, loader, ckpt_path=False):
             val_probs += probs[:, 1].cpu().numpy().tolist()
 
     eer, _ = compute_eer(val_labels, val_probs)
+    accuracy = correct / total
 
-    return val_loss, correct / total, eer
+    return val_loss, accuracy, eer
 
-def train(model, epoch_num, optimizer, ckpt_save_path):
-    print(criterion, optimizer)
+
+def train(model, epoch_num, optimizer, criterion, train_loader, val_loader, ckpt_save_path, lr_scheduler=None):
+    
+    wandb.watch(model, criterion, log="all", log_freq=1)
     min_val_eer = np.inf
 
     for epoch in range(epoch_num):
-        print(epoch)
-        train_loss = 0.0
 
-        for batch in train_loader:
-            inputs, labels = batch[0].to(device), batch[1].to(device)
+        train_loss = 0.0
+        model.train()
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
             optimizer.zero_grad()
+
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
+
             optimizer.step()
+
             train_loss += loss.item()
-        print('_______')
-        val_loss, val_accuracy, val_eer = eval(model, val_loader)
-        test_loss, test_accuracy, test_eer = eval(model, test_loader)
 
-        print('\033[1m' + f'Epoch {epoch}:' + '\033[0m' + f' train loss = {train_loss:.4f}')
-        print(f'val loss = {val_loss:.4f}, val accuracy = {val_accuracy:.4f}, val eer = {val_eer:.4f}')
-        print(f'test loss = {test_loss:.4f}, test accuracy = {test_accuracy:.4f}, test eer = {test_eer:.4f}')
+        val_loss, val_accuracy, val_eer = evaluate(model, val_loader, criterion, device)
 
-        if (val_eer < min_val_eer) or (val_eer < .02):
+        wandb.log({"epoch": epoch, "loss": val_loss, 'val_accuracy': val_accuracy, 'val_eer': val_eer})
+
+        print(f"Epoch {epoch}: train loss = {train_loss:.4f}")
+        print(f"val loss = {val_loss:.4f}, val accuracy = {val_accuracy:.4f}, val eer = {val_eer:.4f}")
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        if val_eer < min_val_eer or val_eer < 0.02:
             min_val_eer = val_eer
             torch.save(model.state_dict(), ckpt_save_path)
-            print(f'Saving new weights with current val loss = {val_loss:.4f}, val accuracy = {val_accuracy:.4f}, val eer = {val_eer:.4f}, test eer = {test_eer:.4f}')
+            print(f"Saving weights with val accuracy = {val_accuracy:.4f}, val eer = {val_eer:.4f}")
 
+    return min_val_eer
 
-    return eer
 
 # ________TRAIN_MODEL_______________________________________________________________________________
 
-model = models.wide_resnet50_2(pretrained=True)
-model.fc = nn.Linear(2048, 2)
+wandb.init(project="open_eyes")
+resnet = models.resnet34(pretrained=True)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Define new Linears layers
+class MyNet(torch.nn.Module):
+    def __init__(self):
+        super(MyNet, self).__init__()
+        self.resnet = torch.nn.Sequential(*list(resnet.children())[:-1])
+        self.fc1 = torch.nn.Linear(resnet.fc.in_features, 256)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(256, 64)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+model = MyNet()
+print(model)
+
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), 
-                      lr=0.0008, 
-                      momentum=0.9, 
-                      nesterov=True, 
-                      weight_decay=0.002)
+optimizer = optim.Adam(model.parameters(),  lr=0.0001)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.95)
 
-_ = train(model, 80, optimizer, './wide_resnet50_2.pth')
+train(model, 100, optimizer,criterion, train_loader, val_loader, './resnet34.pth', lr_scheduler=scheduler)
